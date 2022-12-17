7692a25e204bc741f27b937666612a9a63e96de5
Sadiq Khoja <sadiqkhoja@gmail.com>
2022-12-17 17:20:55 -0500

    Created project

---
+++ iic-odk-auth/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using iic_odk_auth.Models;
+
+namespace iic_odk_auth.Controllers;
+
+public class AuthController : Controller
+{
+    private readonly ILogger<AuthController> _logger;
+
+    public AuthController(ILogger<AuthController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ iic-odk-auth/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace iic_odk_auth.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
